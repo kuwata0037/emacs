807516a13d084a6fc4d76f22eb5dc757c28c2b78
kuwata <kuwata.cu@gmail.com>
2015-03-20 22:17:02 +0900

    use use-package to exec-path-from-shell

--- config/00-path.el
+++ config/00-path.el
@@ -2,5 +2,8 @@
 ;; exec-path-from-shell
 ;; @refer : https://github.com/purcell/exec-path-from-shell
 ;; --------------------------------------------------
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config
+  (exec-path-from-shell-initialize)
+  )
